791fce23fa25c5583c3349bb3b25310574e93394
sansonroot <asanyuo123@gmail.com>
2020-02-22 15:44:09 +0000

    improved solution (give 10 million + scores)

--- main.cpp
+++ main.cpp
@@ -13,19 +13,20 @@ struct comparator{
     }
 };
 
+int pickBook(int index, vector<pair<int,int>> arr,unordered_map<int,int> states);
 
 struct LibraryData{
     int books=0;
     int sign_up_days=0;
     int books_per_day=0;
     bool scan= false;
-    vector<int>scannable_books;
+    vector<pair<int,int>>scannable_books;
 };
 
 int main() {
 
-    ifstream is("../b_read_on.txt");
-    ofstream os("../o_b_read_on.txt");
+    ifstream is("../f_libraries_of_the_world.txt");
+    ofstream os("../o_f_libraries_of_the_world.txt");
 
     int books_n,lib_n,scan_n,val;
 
@@ -38,6 +39,8 @@ int main() {
     //contains the books and their values
     vector<pair<int, int>> vpair;
 
+    vector<int> bookValues;
+
     //storing them in unordered map
     unordered_map<int, int> unorderedMap;
 
@@ -49,13 +52,13 @@ int main() {
 
         is>>val;
 
-//        cout<<val<<endl;
+        bookValues.push_back(val);
 //
-        pair<int, int> p {val,i};
-
-        vpair.push_back(p);
-
-        unorderedMap[val]=i;
+//        pair<int, int> p {val,i};
+//
+//        vpair.push_back(p);
+//
+//        unorderedMap[val]=i;
 
     }
 
@@ -71,13 +74,15 @@ int main() {
 
         LibraryData ld{};
 
-        vector<int> tmp{};
+        vector<pair<int,int>> tmp{};
 
         for (int i = 0; i < books; ++i) {
             int tp;
             is>>tp;
 
-            tmp.push_back(tp);
+            pair<int, int> p {tp,bookValues[tp]};
+
+            tmp.push_back(p);
         }
 
         if (sign_d>=scan_n) {
@@ -108,15 +113,17 @@ int main() {
     }
 
     //sort books
-    sort(vpair.begin(),vpair.end(),comparator());
+//    sort(vpair.begin(),vpair.end(),comparator());
 
 //    cout<<vpair[0].second<<endl;
 
 //    os<<libraries_to_sign_up.size();
 
+    unordered_map<int, int> tracks;
+
     priority_queue <int> pq;
 
-    unordered_map<int, LibraryData> output{};
+    unordered_map<int, vector<int>> output{};
 
     int counter=0;
 
@@ -124,51 +131,85 @@ int main() {
 
         if (r.second.scan){
 
-            int days = r.second.books/r.second.books_per_day+r.second.books%r.second.books_per_day+r.second.sign_up_days;
+            int quotient = r.second.books/r.second.books_per_day;
+
+            int days =  quotient +r.second.sign_up_days;
+
+            int d2 = r.second.books/r.second.books_per_day+r.second.sign_up_days;
 
             //all will be scanned
 
             if (days+1<=scan_n){
 
-                output[r.first]=r.second;
+                vector<int> out{};
+
+                for (int i = 0; i <r.second.scannable_books.size() ; ++i) {
+                    out.push_back(r.second.scannable_books[i].first);
+
+//                    tracks[r.second.scannable_books[i].first]=1;
+                }
+
+                output[r.first]=out;
 
 //                os<<endl;
 
 //                os<<r.first<<" "<<r.second.scannable_books.size()<<endl;
 
-                for (int i = 0; i < r.second.scannable_books.size(); ++i) {
+//                for (int i = 0; i < r.second.scannable_books.size(); ++i) {
 
 //                    os<<r.second.scannable_books[i];
 
-                    if (i+1<r.second.scannable_books.size()){
+//                    if (i+1<r.second.scannable_books.size()){
 //                        os<<" ";
-                    }
-
-                }
+//                    }
+//
+//                }
 
                 counter++;
 
+
             }else{
 
-                //create sorted pairs
-
-                for (int i = 0; i <r.second.scannable_books.size() ; ++i) {
-
-                    int first = r.second.scannable_books[i];
-
-                    pq.push(vpair[first].second);
-
-                }
+                //create sort pairs
+                sort(r.second.scannable_books.begin(),r.second.scannable_books.end(),comparator());
 
                 vector<int> out{};
 
+                int it=0;
+
+//                cout<<"========================"<<endl;
+//                cout<<r.first<<" "<<r.second.scannable_books.size()<<endl;
+//                cout<<"========================"<<endl;
+
                 for (int j = 0; j < scan_n-(r.second.sign_up_days+1) ; ++j) {
 
-                    if(unorderedMap.find(pq.top()) != unorderedMap.end()){
-                        out.push_back(unorderedMap[pq.top()]);
+                    for (int i = 0; i < r.second.books_per_day; ++i) {
 
-                        pq.pop();
+//                        cout<<"At Counter "<<it<<endl;
+//
+//                        if (it>=r.second.scannable_books.size())break;
+//
+//                        int index = pickBook(it,r.second.scannable_books,tracks);
+//
+//                        cout<<"We dont get here "<<index<<endl;
+//
+//                        if (index<0){
+//
+//                            cout<<"Item not found "<<it<<endl;
+//
+//                            index = r.second.scannable_books[it].first;
+//                        }
+
+                        out.push_back(r.second.scannable_books[it].first);
+//                        tracks[index]=1;
+
+                        it++;
+
                     }
+
+                    if (it>=r.second.scannable_books.size())break;
+
+//                    cout<<"Inner loop exits j at "<<j<<endl;
 
                 }
 
@@ -176,32 +217,13 @@ int main() {
 
                     counter++;
 
-//                    os<<endl;
-
-//                    os<<r.first<<" "<<out.size();
-
-                    vector<int> outV;
-
-                    for (int k = 0; k <out.size() ; ++k) {
-
-                        outV.push_back(out[k]+1);
-
-//                        os<<(out[k]);
-
-                        if (k+1<out.size()){
-//                            os<<" ";
-                        }
-
-                    }
-
-                    LibraryData lld;
-
-                    lld.scannable_books = outV;
-                    output[r.first]=lld;
+                    output[r.first] = out;
 
                 }
 
             }
+
+//            scan_n -=r.second.sign_up_days;
 
 
         }
@@ -211,16 +233,17 @@ int main() {
 
     os<<counter;
 
-
     for (auto r: output){
 
         os<<endl;
 
-        os<<r.first<<" "<<r.second.scannable_books.size()<<endl;
-        for (int i = 0; i <r.second.scannable_books.size() ; ++i) {
-            os<<r.second.scannable_books[i];
+        os<<r.first<<" "<<r.second.size()<<endl;
 
-            if (i+1<r.second.scannable_books.size()){
+        for (int i = 0; i <r.second.size() ; ++i) {
+
+            os<<r.second[i];
+
+            if (i+1<r.second.size()){
                 os<<" ";
             }
 
@@ -233,4 +256,20 @@ int main() {
     cout<<"Done "<<counter;
 
     return 0;
+}
+
+
+int pickBook(int index, vector<pair<int,int>> arr,unordered_map<int,int> states){
+
+    if (index>=arr.size())
+        return -1;
+
+    int it = arr[index].first;
+
+    if (states.find(it)==states.end()) return it;
+
+    cout<<"Executing recursion "<<index<<endl;
+
+    return pickBook(index+1,arr,states);
+
 }
